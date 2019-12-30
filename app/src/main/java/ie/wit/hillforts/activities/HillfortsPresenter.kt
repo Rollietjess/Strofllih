64c37a47f721088760080a15d01a52fd5827b6eb
Rollietjes <rollietjes@gmail.com>
2019-12-30 10:07:11 +0100

    Present class

---
+++ app/src/main/java/ie/wit/hillforts/activities/HillfortsPresenter.kt
@@ -0,0 +1,76 @@
+package ie.wit.hillforts.activities
+
+import android.content.Intent
+import org.jetbrains.anko.intentFor
+import ie.wit.hillforts.helpers.showImagePicker
+import ie.wit.hillforts.main.MainApp
+import ie.wit.hillforts.models.Location
+import ie.wit.hillforts.models.HillfortsModel
+
+class PlacemarkPresenter(val view: HillfortsActivity) {
+
+    val IMAGE_REQUEST = 1
+    val LOCATION_REQUEST = 2
+
+    var hillfort = HillfortsModel()
+    var location = Location(52.245696, -7.139102, 15f)
+    var app: MainApp
+    var edit = false;
+
+    init {
+        app = view.application as MainApp
+        if (view.intent.hasExtra("hillfort_edit")) {
+            edit = true
+            hillfort = view.intent.extras?.getParcelable<HillfortsModel>("hillfort_edit")!!
+//            view.showHillfort(hillfort)
+        }
+    }
+
+    fun doAddOrSave(title: String, description: String) {
+        hillfort.title = title
+        hillfort.description = description
+        if (edit) {
+            app.hillforts.update(hillfort)
+        } else {
+            app.hillforts.create(hillfort)
+        }
+        view.finish()
+    }
+
+    fun doCancel() {
+        view.finish()
+    }
+
+    fun doDelete() {
+        app.hillforts.delete(hillfort)
+        view.finish()
+    }
+
+    fun doSelectImage() {
+        showImagePicker(view, IMAGE_REQUEST)
+    }
+
+    fun doSetLocation() {
+        if (hillfort.zoom != 0f) {
+            location.lat = hillfort.lat
+            location.lng = hillfort.lng
+            location.zoom = hillfort.zoom
+        }
+//        view.startActivityForResult(view.intentFor<EditLocationView>().putExtra("location", location), LOCATION_REQUEST)
+    }
+
+    fun doActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                hillfort.image = data.data.toString()
+//                view.showHillfort(hillfort)
+            }
+            LOCATION_REQUEST -> {
+                location = data.extras?.getParcelable<Location>("location")!!
+                hillfort.lat = location.lat
+                hillfort.lng = location.lng
+                hillfort.zoom = location.zoom
+            }
+        }
+    }
+}
